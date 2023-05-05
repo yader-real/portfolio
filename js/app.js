a879f6fc7f2c5f87dad11a694b490361ebda44b0
yader-real <yader.real@gmail.com>
2023-05-05 09:53:11 -0600

    first version

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const check = document.querySelector(".check");
+check.addEventListener('click', idioma);
+
+function idioma(){
+    let id = (check.checked);
+    if (id == true){
+        location.href = "en/index.html"
+    }else{
+        location.href = "../index.html"
+    }
+
+}
